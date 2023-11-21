99907a700d624df38e474dde196ba28b2ac43cf5
maciek <3296662+erbelion@users.noreply.github.com>
2023-11-21 05:09:52 +0100

    0.2.2

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # showcase-vite-plugin-laravel-purgecss
+
+showcase of [@erbelion/vite-plugin-laravel-purgecss](https://github.com/Erbelion/vite-plugin-laravel-purgecss)@0.2.2 running on laravel@10.2.9
  
+## what's been done here
+
+```
+cd showcase-vite-plugin-laravel-purgecss
+composer create-project laravel/laravel:10.2.9 .
+composer require laravel/ui
+php artisan ui bootstrap
+npm install @erbelion/vite-plugin-laravel-purgecss@0.2.2
+npm run build
+php artisan serve
+```
+
+changed [resources\views\welcome.blade.php](https://github.com/erbelion/vite-plugin-laravel-purgecss/blob/main/resources/views/welcome.blade.php)
+
+changed [vite.config.js](https://github.com/erbelion/vite-plugin-laravel-purgecss/blob/main/vite.config.js)
+
+added but not needed [public\guide.png](https://github.com/erbelion/vite-plugin-laravel-purgecss/blob/main/public/guide.png)
+
+## results
+
+### laravel
+```
+public/build/assets/app-041e359a.css  225.70 kB │ gzip: 30.76 kB
+```
+
+### laravel + @erbelion/vite-plugin-laravel-purgecss
+```
+public/build/assets/app-041e359a.css    9.64 kB │ gzip:  2.86 kB
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="col-lg-8 mx-auto p-4 py-md-5">
+        <main>
+            <h1 class="text-body-emphasis">showcase-vite-plugin-laravel-purgecss@0.2.2</h1>
+            <div class="mb-5">
+                <a href="https://github.com/Erbelion/vite-plugin-laravel-purgecss"
+                    class="btn btn-lg px-4 btn-primary">@erbelion/vite-plugin-laravel-purgecss</a>
+            </div>
+            <p class="fs-5 col-md-8">try replacing <code>btn-primary</code> class of the button above from
+                <code>-primary</code> to
+                <code>-danger</code> in the browser
+            </p>
+            <p class="fs-5 col-md-8">
+                if it doesn't change to <span class="text-danger">this color</span> — the plugin
+                is working
+            </p>
+            <p class="fs-5 col-md-8">
+                remember — it only works in production mode
+            </p>
+            <img src="./guide.png" class="img-fluid">
+        </main>
+    </div>
+</body>
+
+</html>
