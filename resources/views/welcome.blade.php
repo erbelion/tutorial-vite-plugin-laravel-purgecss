602bcfd626054fc3fc2e530f4461e0c06f52ea5c
maciek <3296662+erbelion@users.noreply.github.com>
2024-11-06 18:08:08 +0100

    edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,22 @@
                 <a href="https://github.com/erbelion/vite-plugin-laravel-purgecss"
                     class="btn btn-lg px-4 btn-primary">@erbelion/vite-plugin-laravel-purgecss</a>
             </div>
-            <p class="fs-5 col-md-8">try replacing <code>btn-primary</code> class of the button above from
-                <code>-primary</code> to
-                <code>-danger</code> in the browser
-            </p>
-            <p class="fs-5 col-md-8">
-                if it doesn't change to <span class="text-danger">this color</span> — the plugin
-                is working
-            </p>
-            <p class="fs-5 col-md-8">
-                remember — it only works in production mode
-            </p>
+            <div class="fs-5 col-md-12">
+                <p>try replacing <code>btn-primary</code> class of the button above from
+                    <code>-primary</code> to
+                    <code>-danger</code> in the browser
+                </p>
+                <p>
+                    if it doesn't change to <span class="text-danger">this color</span> — the plugin
+                    is working
+                </p>
+                <p>
+                    remember — it only works in production mode
+                </p>
+                <p id="used_rule_from_app_scss">
+                    also check out app.scss!
+                </p>
+            </div>
             <img src="./guide.png" class="img-fluid">
         </main>
     </div>
