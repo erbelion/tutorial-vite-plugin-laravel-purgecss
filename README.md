9b99b82e2aa113ad794bd47768f609551c2c2528
maciek <3296662+erbelion@users.noreply.github.com>
2025-06-10 17:41:56 +0200

    bump: plugin:0.4.0, laravel/laravel:12.0.9

--- README.md
+++ README.md
@@ -2,54 +2,54 @@
 
 [![views](https://views.erbek.space/api/views.svg?id=github.com%2Ferbelion%2Ftutorial-vite-plugin-laravel-purgecss)](https://views.erbek.space/page/?id=github.com%2Ferbelion%2Ftutorial-vite-plugin-laravel-purgecss) [![likes](https://views.erbek.space/api/likes.svg?id=github.com%2Ferbelion%2Ftutorial-vite-plugin-laravel-purgecss)](https://views.erbek.space/api/like?id=github.com%2Ferbelion%2Ftutorial-vite-plugin-laravel-purgecss)
 
-tutorial for [@erbelion/vite-plugin-laravel-purgecss](https://github.com/Erbelion/vite-plugin-laravel-purgecss)@0.3.4 running on laravel/laravel@11.4.0
+tutorial for [@erbelion/vite-plugin-laravel-purgecss](https://github.com/Erbelion/vite-plugin-laravel-purgecss)@0.4.0 running on laravel/laravel@12.0.9
 
-## how to run this example
+## 🚀 how to run this example
 
 clone this repo
 
-```
+```bash
 composer install
 npm install
 npm run build
 php artisan serve
 ```
 
-## how to do it yourself
+## 🤓 how to do it yourself
 
-```
-composer create-project laravel/laravel:11.4.0 .
+```bash
+composer create-project laravel/laravel:12.0.9 .
 composer require laravel/ui
 php artisan ui bootstrap
 npm install
-npm install @erbelion/vite-plugin-laravel-purgecss@0.3.4
+npm install @erbelion/vite-plugin-laravel-purgecss@0.4.0
 ```
 
 in .env, set `SESSION_DRIVER=file`
 
-overwrite [resources\views\welcome.blade.php](https://github.com/erbelion/tutorial-vite-plugin-laravel-purgecss/blob/main/resources/views/welcome.blade.php)
+overwrite [resources\views\welcome.blade.php](https://raw.githubusercontent.com/erbelion/tutorial-vite-plugin-laravel-purgecss/refs/heads/main/resources/views/welcome.blade.php)
 
-overwrite [vite.config.js](https://github.com/erbelion/tutorial-vite-plugin-laravel-purgecss/blob/main/vite.config.js)
+overwrite [vite.config.js](https://raw.githubusercontent.com/erbelion/tutorial-vite-plugin-laravel-purgecss/refs/heads/main/vite.config.js)
 
-overwrite [resources\sass\app.scss](https://github.com/erbelion/tutorial-vite-plugin-laravel-purgecss/blob/main/resources/sass/app.scss)
+overwrite [resources\sass\app.scss](https://raw.githubusercontent.com/erbelion/tutorial-vite-plugin-laravel-purgecss/refs/heads/main/resources/sass/app.scss)
 
 (not needed) create [public\guide.png](https://github.com/erbelion/tutorial-vite-plugin-laravel-purgecss/blob/main/public/guide.png)
 
-```
+```bash
 npm run build
 php artisan serve
 ```
 
-## results
+## 📊 results
 
 ### laravel
 
 ```
-public/build/assets/app-CLUG7ZNT.css  230.78 kB │ gzip: 31.45 kB
+public/build/assets/app-BtR4ZoVk.css  227.48 kB │ gzip: 31.27 kB
 ```
 
 ### laravel + @erbelion/vite-plugin-laravel-purgecss
 
 ```
-public/build/assets/app-CLUG7ZNT.css   10.04 kB │ gzip:  3.00 kB
+public/build/assets/app-BtR4ZoVk-b7997f51.css   10.18 kB │ gzip:  3.06 kB
 ```
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,46 @@
-<!doctype html>
-<html lang="en" data-bs-theme="auto">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
+        <title>Laravel</title>
 
-<body>
-    <div class="col-lg-8 mx-auto p-4 py-md-5">
-        <main>
-            <h1 class="text-body-emphasis">tutorial-vite-plugin-laravel-purgecss@0.3.4</h1>
-            <div class="mb-5">
-                <a href="https://github.com/erbelion/vite-plugin-laravel-purgecss"
-                    class="btn btn-lg px-4 btn-primary">@erbelion/vite-plugin-laravel-purgecss</a>
-            </div>
-            <div class="fs-5 col-md-12">
-                <p>try replacing <code>btn-primary</code> class of the button above from
-                    <code>-primary</code> to
-                    <code>-danger</code> in the browser
-                </p>
-                <p>
-                    if it doesn't change to <span class="text-danger">this color</span> — the plugin
-                    is working
-                </p>
-                <p>
-                    remember — it only works in production mode
-                </p>
-                <p id="used_rule_from_app_scss">
-                    also check out resources/sass/app.scss!
-                </p>
-            </div>
-            <img src="./guide.png" class="img-fluid">
-        </main>
-    </div>
-</body>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @endif
+    </head>
+    <body>
+        <div class="col-lg-8 mx-auto p-4 py-md-5">
+            <main>
+                <h1 class="text-body-emphasis">tutorial-vite-plugin-laravel-purgecss@0.3.4</h1>
+                <div class="mb-5">
+                    <a href="https://github.com/erbelion/vite-plugin-laravel-purgecss"
+                        class="btn btn-lg px-4 btn-primary">@erbelion/vite-plugin-laravel-purgecss</a>
+                </div>
+                <div class="fs-5 col-md-12">
+                    <p>try replacing <code>btn-primary</code> class of the button above from
+                        <code>-primary</code> to
+                        <code>-danger</code> in the browser
+                    </p>
+                    <p>
+                        if it doesn't change to <span class="text-danger">this color</span> — the plugin
+                        is working
+                    </p>
+                    <p>
+                        remember — it only works in production mode
+                    </p>
+                    <p id="used_rule_from_app_scss">
+                        also check out resources/sass/app.scss!
+                    </p>
+                </div>
+                <img src="./guide.png" class="img-fluid">
+            </main>
+        </div>
+    </body>
 </html>
